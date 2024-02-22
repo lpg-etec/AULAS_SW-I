8e66de3382c5f911a71a23e6438493013b10f3fe
lpg-etec <luiggy.garcia@etec.sp.gov.br>
2024-02-22 12:29:46 -0300

    lamp

---
+++ Lampada/ConsoleApp2/Lampada.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    public class Lampada
+    {
+        public bool Estado { get; set; }
+        public string Cor { get; set; }
+        public int Potencia { get; set; }
+        public Lampada() {
+
+        }
+    }
+}
---
+++ Lampada/ConsoleApp2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Lampada l1 = new Lampada();
+            l1.Cor = "Azul";
+            l1.Estado = true;
+            l1.Potencia = 250;
+            Console.WriteLine($"Cor: {l1.Cor}\nEstado: {l1.Estado}\nPotência: {l1.Potencia}");
+        }
+    }
+}
