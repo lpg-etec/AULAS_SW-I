b0565767467fdb14de0656707af30cefb4220ea9
lpg-etec <luiggy.garcia@etec.sp.gov.br>
2024-03-01 12:40:33 -0300

    l

---
+++ ProjetoLampada/ProjetoLampada/ProjetoLampada/Lampada.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ProjetoLampada
+{
+    public class Lampada
+    {
+        private bool ligada { get; set; }
+        private int potencia { get; set; }
+        private string cor { get; set; }
+
+        public Lampada(bool l, int p, string c)
+        {
+            this.ligada = l;
+            this.potencia = p;
+            this.cor = c;
+        }
+
+        public void ligar()
+        {
+            this.ligada = true;
+        }
+        public void desligada()
+        {
+            this.ligada = false;
+        }
+        public bool estaLigado()
+        {
+            return this.ligada;
+        }
+    }
+}
---
+++ ProjetoLampada/ProjetoLampada/ProjetoLampada/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace ProjetoLampada
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Lampada lamp1 = new Lampada(false, 1000, "Vermelho");
+            lamp1.ligar();
+            Console.WriteLine($"ligada: {lamp1.estaLigado()}");
+        }
+    }
+}
