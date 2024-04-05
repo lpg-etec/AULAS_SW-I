0f257b0a7c85d593f90aa1c0e3f6cdc7452ddef1
lpg-etec <luiggy.garcia@etec.sp.gov.br>
2024-04-05 12:04:54 -0300

    q

---
+++ Quadrado/Quadrado/Area.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quadrado
+{
+    class Area
+    {
+        public int Base { get; set; }
+        public int Altura { get; set; }
+        public Area(int lado, int altura) 
+        {
+            this.Base = lado;
+            this.Altura = altura;
+        }
+        public int Calcular()
+        {
+            return this.Base * this.Altura;
+        }
+    }
+}
---
+++ Quadrado/Quadrado/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+
+namespace Quadrado
+{
+    class main 
+    {
+        public static void Main(String[] args)
+        {
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            int n2 = Convert.ToInt32(Console.ReadLine());
+            Area a = new Area(n1, n2);
+            string c = a.Base == a.Altura ? "Quadrado" : "Retângulo";
+            Console.WriteLine($"{a.Base} * {a.Altura} = {a.Calcular()} ({c})");
+        }
+    }
+}
