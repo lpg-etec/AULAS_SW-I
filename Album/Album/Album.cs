093f693cf2d636043cb3687b804df9ab842c3477
lpg-etec <luiggy.garcia@etec.sp.gov.br>
2024-04-11 08:42:04 -0300

    l: album

---
+++ Album/Album/Album.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Program
+{
+    class Album
+    {
+        readonly int numeroTotalDePaginas = 2;
+        int numeroTotalDeFotos;
+        int numeroDeFotos;
+        public int NumeroDeFotos
+        {
+            get { return numeroDeFotos; }
+            set { if (value > numeroTotalDeFotos) { throw new IndexOutOfRangeException(); } }
+        }
+        public Album(int total, int numero)
+        {
+            this.numeroTotalDeFotos = total;
+            this.numeroDeFotos = numero;
+        }
+    }
+}
---
+++ Album/Album/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Album album = new Album(5, 2);
+            Console.WriteLine($"{album.NumeroDeFotos}");
+            Album album1 = new Album(5, 2);
+            album1.NumeroDeFotos = 6;
+            Console.WriteLine($"{album1.NumeroDeFotos}");
+        }
+    }
+}
